feed7e9b3dd2da02850c1ac1419545eb89d998a5
Denis Churbanov <4urbanoff@gmail.com>
2022-07-20 16:16:30 +0300

    update root page

--- src/bundle.tsx
+++ src/bundle.tsx
@@ -1,7 +1,7 @@
 /// <reference path="./@types/index.d.ts" />
 
-import './reset.css'
-import { Route } from 'whatsup/route'
+import styles, { Div } from './bundle.scss'
+import { Route, RouteLink } from 'whatsup/route'
 import { render } from 'whatsup/jsx'
 import { App as Todos } from './todos'
 import { App as Sierpinski } from './sierpinski'
@@ -9,11 +9,25 @@ import { App as Loadable } from './loadable'
 
 function App() {
     return (
-        <>
+        <Div container>
             <Route path="/todos" component={Todos} />
             <Route path="/sierpinski" component={Sierpinski} />
             <Route path="/loadable" component={Loadable} />
-        </>
+            <Div default>
+                <Div header>Whatsup examples</Div>
+                <Div flex>
+                    <RouteLink className={styles.button + ' ' + styles.blue} to="/todos">
+                        Todos
+                    </RouteLink>
+                    <RouteLink className={styles.button + ' ' + styles.orange} to="/loadable">
+                        Loadable
+                    </RouteLink>
+                    <RouteLink className={styles.button + ' ' + styles.green} to="/sierpinski">
+                        Sierpinski
+                    </RouteLink>
+                </Div>
+            </Div>
+        </Div>
     )
 }
 
